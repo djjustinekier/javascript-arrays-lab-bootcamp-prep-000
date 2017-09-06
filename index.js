a3bfd85c020ba4b73373f5ca9728ae662bf79cc8
Don Justine Fontanilla <djjustinekier@gmail.com>
2017-09-06 15:51:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ const app = "I don't do much."
 var kittens = ['Milo', 'Otis', 'Garfield'];
 
 function destructivelyAppendKitten(name) {
-    return kittens.push(name)
+    return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
 }
